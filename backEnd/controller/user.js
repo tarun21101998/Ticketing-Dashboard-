59cd77fd4599b96d429bdc23f2d0307bee385ada
tarun <ts8295437364@gmail.com>
2023-09-26 17:33:43 +0530

    26/09/2023

--- backEnd/controller/user.js
+++ backEnd/controller/user.js
@@ -1,28 +1,83 @@
-    const collection=  require('../modals/data')
+const collection=  require('../modals/data')
+const collection1 = require('../modals/loginData')
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'e-com';
+
 
 // creating user
 module.exports.createData= async (req, resp)=>{
+
+  try {
+    const existingUser = await collection.findOne({ email: req.body.email });
     
-      try {
-        const existingUser = await collection.findOne({ email: req.body.email });
-        
-        if (!existingUser) {
-          const newUser = await collection.create(req.body);
-          return resp.redirect('https://www.google.com');
-        } else {
-         return resp.status(401).json({ error: 'Unauthorized' });
-        }
-      } catch (err) {
-        console.log('Error in signing up:', err);
-        return res.redirect('back');
-      }
-  
+    if (!existingUser) {
+      const newUser = await collection.create(req.body);
+      return resp.send("data added");
+    } else {
+    
+      return resp.status(401).json({ error: 'Unauthorized' });
+    }
+  } catch (err) {
+    console.log('Error in signing up:', err);
+    return res.redirect('back');
+  }
+
 }
 
 // fetching the data of user
 module.exports.getData = async (req, resp)=>{
-    const result = await collection.find({})
-    // console.log(result)
-    resp.send(result);
+const result = await collection.find({})
+// console.log(result)
+resp.send(result);
 }
 
+
+
+module.exports.registerData= async (req, resp)=>{
+
+try {
+let user = new collection1(req.body);
+let result = await user.save();
+result = result.toObject();
+delete result.password
+Jwt.sign({result}, jwtKey, {expiresIn:"2h"},(err,token)=>{
+    if(err){
+        return resp.send("Something went wrong")  
+    }
+    console.log(token)
+    return resp.send({result,auth:token})
+})
+}
+catch (err) {
+console.log('Error in signing up:', err);
+return resp.redirect('back');
+}
+
+}
+
+
+module.exports.loginData= async (req, resp)=>{
+
+try {
+if (req.body.password && req.body.email) {
+  let user = await collection1.findOne(req.body).select("-password");
+  if (user) {
+      Jwt.sign({user}, jwtKey, {expiresIn:"2h"},(err,token)=>{
+          if(err){
+              resp.send("Something went wrong")  
+          }
+          resp.send({user,auth:token})
+      })
+  } else {
+      resp.send({ result: "No User found" })
+  }
+} else {
+  resp.send({ result: "No User found" })
+}
+}
+catch (err) {
+console.log('Error in signing up:', err);
+return resp.redirect('back');
+}
+
+}
--- backEnd/routes/user.js
+++ backEnd/routes/user.js
@@ -2,8 +2,11 @@
 const express = require('express')
 const router = express.Router();
 const user = require('../controller/user')
+
 // Data sending to Data Base
 router.post('/users', user.createData)
+router.post('/register', user.registerData)
+router.post('/login', user.loginData)
 
 // Data is fetching from DataBase
 router.get('/users', user.getData)
