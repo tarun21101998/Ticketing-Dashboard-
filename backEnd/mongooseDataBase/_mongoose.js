eedc1c687c2327e3acf49d3f115ae381d3a8dead
tarun <ts8295437364@gmail.com>
2023-10-16 20:37:42 +0530

    commit

--- backEnd/mongooseDataBase/_mongoose.js
+++ backEnd/mongooseDataBase/_mongoose.js
@@ -4,6 +4,6 @@ const dataBase = require('../config/env')
 // console.log(url.url)
 mongoose.connect('mongodb://'+dataBase.url+dataBase.dataBaseName)
 .then(()=>{console.log("DataBase connection is successful")})
-.catch((err)=>{console.log("error is showing")})
+.catch((err)=>{console.log("error is showing in dataBase")})
 
 
